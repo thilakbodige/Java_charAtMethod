39163a82a4b80edf6247f30c96609407cb7f8d2a
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-11 12:41:21 +0530

    Add files via upload

---
+++ Find_Length.java
@@ -0,0 +1,15 @@
+package com.charAtMethod;
+import java.util.*;
+
+public class Find_Length 
+{
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the String");
+	String s=sc.next();
+	int size=s.length();
+	System.out.println("The Size of the String is:"+size);
+	}
+
+}
