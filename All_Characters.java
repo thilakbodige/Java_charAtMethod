3e12a46465441273b192a2615c98866bf91a6e46
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-11 12:42:03 +0530

    Add files via upload

---
+++ All_Characters.java
@@ -0,0 +1,16 @@
+package com.charAtMethod;
+import java.util.*;
+public class All_Characters 
+{
+   public static void main(String[] args) 
+{
+    Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the String");
+		String s=sc.next();
+		for(int i=0;i<=(s.length())-1;i++);
+		{
+			
+		System.out.println(s.charAt(0));
+		}
+	}
+	}
