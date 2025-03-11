9320672f8cc1792c58eed454eca331fea66a1022
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-11 12:41:39 +0530

    Add files via upload

---
+++ Extract_Character.java
@@ -0,0 +1,14 @@
+package com.charAtMethod;
+import java.util.*;
+public class Extract_Character 
+{
+	public static void main(String[] args)
+	{
+	   Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the String");
+       String s=sc.next();
+       char ch=s.charAt(3);
+       System.out.println(ch);
+	}
+
+}
